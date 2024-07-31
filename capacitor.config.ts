b0f6b797db00758aa41aab8e5b02e06adb77279c
satsangamusa <33586258+satsangamusa@users.noreply.github.com>
2024-07-31 19:50:06 -0400

    adjust image for german, zoom feature for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.threesouls.english.bible',
   appName: 'Bible Gems',
-  webDir: 'www/browser'
+  webDir: 'www/browser',
+  zoomEnabled:true
 };
 
 export default config;
